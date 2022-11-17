75592989373bcdb8b4cd99ef08dcd5e11ea3f302
chansub <chansub@users.noreply.github.com>
2022-11-17 04:59:31 +0000

    deploy: d3f662febccf007abe6cfd2987c543e306e8e33b

--- src/components/generic/DisplayRounds.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DisplayRounds = (props) => {
-    return (
-        <><span>Rounds: </span><span className="numbers rounds">{props.rounds}</span></>
-    );
-};
-
-export default DisplayRounds;
