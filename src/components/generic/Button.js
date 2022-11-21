9dab680efc27ea5103785baafd55dac531687493
Susana <steymourian@hbs.edu>
2022-11-20 23:48:52 -0500

    Update Button.js

--- src/components/generic/Button.js
+++ src/components/generic/Button.js
@@ -11,7 +11,7 @@ const imgSources = [
   {type: 'pause', src: pauseIcon},
   {type: 'reset', src: resetIcon},
   {type: 'stop', src: stopIcon},
-  {type: 'remove', src: removeIcon}
+  {type: 'remove', src: removeIcon, width: '20px'}
 ]
 
 const Button = ({ type, text, ...btnProps }) => {
@@ -19,7 +19,7 @@ const Button = ({ type, text, ...btnProps }) => {
   function renderButton(buttonType,buttonText){
     let item = imgSources.find(item => item.type === buttonType);
     if(item){
-      return <img src={item.src} width="30" alt={buttonText} title={buttonText}/>
+      return <img src={item.src} width={item.width ? item.width : '30px'} alt={buttonText} title={buttonText}/>
     }
     else{
       return buttonText;
